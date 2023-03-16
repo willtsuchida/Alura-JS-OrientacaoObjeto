d2cf95a01c945a15f9816136337acb699d099931
willtsuchida <willian_hideki@hotmail.com>
2023-03-16 16:18:20 -0300

    capitulo 2 finalizado, Curso de JavaScript: programando a Orientação a Objetos

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
-    saldo;
+    _saldo = 0;
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor) {
+        if (valor <= 0) {
+            return;
+        }
+        this._saldo += valor;
+    }
 }
 
 const cliente1 = new Cliente();
@@ -10,17 +27,18 @@ const cliente2 = new Cliente();
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 111222333309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 cliente2.nome = "Alice";
 cliente2.cpf = 888222333309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-const cliente3Nome = "Alice";
-const cliente3CPF = 888222333309;
-const cliente3Agencia = 1001;
-const cliente3Saldo = 0;
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
 
-console.log(cliente1, cliente2);
+contaCorrenteRicardo.depositar(-100.0)
+contaCorrenteRicardo.depositar(100.0)
+contaCorrenteRicardo.depositar(100.0)
+const valorSacado = contaCorrenteRicardo.sacar(50);
+
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);

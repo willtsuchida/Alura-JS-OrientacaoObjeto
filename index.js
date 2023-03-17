68bbf4b36685b252de23b68f362b674a21c5a56f
willtsuchida <willian_hideki@hotmail.com>
2023-03-17 11:50:56 -0300

    cap 4 finalizado, Curso de JavaScript: programando a Orientação a Objetos

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
+
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
 
@@ -15,10 +16,11 @@ contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
+conta2.cliente = cliente1;
 conta2.agencia = 102;
 
-contaCorrenteRicardo.transferir(200, conta2);
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
 
-console.log(contaCorrenteRicardo);
-console.log(conta2);
+conta2.saldo = 30000;
+console.log(conta2.saldo);
